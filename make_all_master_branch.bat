160c3311b6dc947dde2906080bb8d533cbf10fbc
Lohkdesgds <lohk.lohkdesgds@gmail.com>
2024-04-24 18:46:05 -0300

    [@Lohkdesgds] Fixed references and added auto master branch script

---
+++ make_all_master_branch.bat
@@ -0,0 +1,6 @@
+@echo off
+echo Just doing git checkout master on everything and pulling...
+git checkout master
+git pull
+git submodule foreach "git checkout master; git pull origin master; git submodule foreach git checkout master; git pull origin master"
+set /p idc=Ended! You can close this now.
