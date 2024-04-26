1d424446fbbae8587548b4c278b48ab38f0c7659
Lohkdesgds <lohk.lohkdesgds@gmail.com>
2024-04-26 20:18:35 -0300

    [@Lohkdesgds] Fixed ref module and possibly batch for submodule init

--- recursive_submodule_init.bat
+++ recursive_submodule_init.bat
@@ -1,5 +1,6 @@
 @echo off
 echo Updating every single list of submodules
 git pull
-git submodule foreach "git submodule update --init --recursive; git submodule foreach git submodule update --init --recursive; git submodule foreach git submodule update --init --recursive;"
+git submodule update --init --recursive
+rem git submodule foreach "git submodule update --init --recursive; git submodule foreach git submodule update --init --recursive; git submodule foreach git submodule update --init --recursive;"
 set /p idc=Ended! You can close this now.
